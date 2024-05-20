480d5df2352c54625ac200908b5786cc95acdd1d
Maybe-HZ <ahmadsaifulmutaqiazis1@gmail.com>
2024-05-20 20:58:09 +0700

    JS12_C

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,25 +6,32 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    public function fileUpload(){
+    public function fileUpload()
+    {
         return view('fileUpload');
     }
-    public function prosesFileUpload(Request $request){
+    public function prosesFileUpload(Request $request)
+    {
         // return "Pemorosesan file upload disini";
-        if($request->hasFile('file')){
-            echo "path() : " . $request->file->Path();
-            echo "<br>";
-            echo "extention() : " . $request->file->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension() : " . $request->file->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType() : " . $request->file->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName() : " . $request->file->getClientOriginalName();
-            echo "<br>";
-            echo "getSize() : " . $request->file->getSize();
-        }else{
-            echo "File tidak ada";
-        }
+        // if($request->hasFile('file')){
+        //     echo "path() : " . $request->file->Path();
+        //     echo "<br>";
+        //     echo "extention() : " . $request->file->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension() : " . $request->file->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType() : " . $request->file->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName() : " . $request->file->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize() : " . $request->file->getSize();
+        // }else{
+        //     echo "File tidak ada";
+        // }
+
+        $request->validate([
+            'file' => 'required|file|image|max:5000'
+        ]);
+        echo $request->file('file')->getClientOriginalName()." lolos validasi";
     }
 }
